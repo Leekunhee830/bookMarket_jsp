d6a74261ef82f1410806041b8d6559bb4dd795df
Leekunhee830 <gorrka159@naver.com>
2021-09-12 23:40:05 +0900

    Jsp bookMarket
    추가: productDto 추가
    
    수정일: 2021-09-12

---
+++ bookMarket/src/main/java/com/bookmarket/dto/ProductDto.java
@@ -0,0 +1,76 @@
+package com.bookmarket.dto;
+
+public class ProductDto {
+	private int pd_num;
+	private String pd_code;
+	private String pd_name;
+	private String pd_contents;
+	private int pd_price;
+	private int pd_amount;
+	private String pd_category;
+	private String pd_manufacturer;
+	private int pd_views;
+	private String pd_regdate;
+	
+	public int getPd_num() {
+		return pd_num;
+	}
+	public void setPd_num(int pd_num) {
+		this.pd_num = pd_num;
+	}
+	public String getPd_code() {
+		return pd_code;
+	}
+	public void setPd_code(String pd_code) {
+		this.pd_code = pd_code;
+	}
+	public String getPd_name() {
+		return pd_name;
+	}
+	public void setPd_name(String pd_name) {
+		this.pd_name = pd_name;
+	}
+	public String getPd_contents() {
+		return pd_contents;
+	}
+	public void setPd_contents(String pd_contents) {
+		this.pd_contents = pd_contents;
+	}
+	public int getPd_price() {
+		return pd_price;
+	}
+	public void setPd_price(int pd_price) {
+		this.pd_price = pd_price;
+	}
+	public int getPd_amount() {
+		return pd_amount;
+	}
+	public void setPd_amount(int pd_amount) {
+		this.pd_amount = pd_amount;
+	}
+	public String getPd_category() {
+		return pd_category;
+	}
+	public void setPd_category(String pd_category) {
+		this.pd_category = pd_category;
+	}
+	public String getPd_manufacturer() {
+		return pd_manufacturer;
+	}
+	public void setPd_manufacturer(String pd_manufacturer) {
+		this.pd_manufacturer = pd_manufacturer;
+	}
+	public int getPd_views() {
+		return pd_views;
+	}
+	public void setPd_views(int pd_views) {
+		this.pd_views = pd_views;
+	}
+	public String getPd_regdate() {
+		return pd_regdate;
+	}
+	public void setPd_regdate(String pd_regdate) {
+		this.pd_regdate = pd_regdate;
+	}
+	
+}
